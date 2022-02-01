a9fcb73a2db262cce3fe1beb714ba0c9b1598ad7
andi <sepphirnoederl@gmail.com>
2022-02-01 13:31:11 +0100

    minor changes

--- components/NetworkOverview/AddDeviceDialog.tsx
+++ components/NetworkOverview/AddDeviceDialog.tsx
@@ -8,13 +8,13 @@ import {
 import { NetworkData } from './NetworkCard';
 
 enum State {
-  Connecting,
-  Connected,
+  ConnectWifi,
+  SelectWifiNetwork,
 }
 
 const instructions: Array<string> = [];
-instructions[State.Connecting] = "Please connect to the open WiFi access point of the device. Do not leave this site.";
-instructions[State.Connected] = "Successfully connected to the device. Click continue to initialize the device.";
+instructions[State.ConnectWifi] = "Please connect to the open WiFi access point of the device.";
+instructions[State.SelectWifiNetwork] = "Select a WiFi network for the new device.";
 
 export default function AddDeviceDialog({ open, setOpen, networkData }:
   {
@@ -24,58 +24,22 @@ export default function AddDeviceDialog({ open, setOpen, networkData }:
   }) {
 
   const [error, setError] = React.useState("");
-  const [state, setState] = React.useState(State.Connecting);
+  const [state, setState] = React.useState(State.ConnectWifi);
   const [ssid, setSsid] = React.useState("A1-BED564");
   const [pass, setPass] = React.useState("5AUXYAQL4F");
 
   React.useEffect(() => {
-
-    const timeout = () => {
-      let controller = new AbortController();
-      setTimeout(() => controller.abort(), 2000);
-      return controller.signal;
-    };
-
-    let canceled = false;
-
-    const ping = async () => {
-      console.log("Start pinging.");
-
-      if (!(networkData.refresh_token && networkData.network_id)) {
-        setError("The network was not set up correctly.");
-        return;
-      }
-
-      while (!canceled) {
-        try {
-          const res = await fetch("http://172.16.0.1", { signal: timeout() });
-          console.log("Successful ping: " + res);
-          setError("");
-          setState(State.Connected);
-          break;
-        }
-        catch (e) {
-          console.log(e);
-        }
-        await new Promise(r => setTimeout(r, 1000));
-      }
-    }
-
     if (!open)
-      setState(State.Connecting);
+      setState(State.ConnectWifi);
     else {
       switch (state) {
-        case State.Connecting:
+        case State.ConnectWifi:
           setError("");
-          ping();
           break;
-        case State.Connected:
+        case State.SelectWifiNetwork:
           break;
       }
     }
-
-    return () => { canceled = true; }
-
   }, [state, networkData.network_id, networkData.refresh_token, open]);
 
   const handleClose = () => {
@@ -88,17 +52,23 @@ export default function AddDeviceDialog({ open, setOpen, networkData }:
         <DialogTitle>Add a Device</DialogTitle>
         <DialogContent>
           <Stack gap='40px' alignItems='center'>
-            <Alert severity={state == State.Connected ? 'success' : 'info'}>
+            <Alert severity='info'>
               {instructions[state]}
             </Alert>
-            {state == State.Connecting && <CircularProgress />}
             {error && <Alert severity='error'>{error}</Alert>}
           </Stack>
         </DialogContent>
         <DialogActions>
           <Stack direction='row' gap='20px'>
             <Button onClick={handleClose}>Cancel</Button>
-            {state == State.Connected
+            {state == State.ConnectWifi
+              && <Button
+                onClick={handleClose}
+                href={`http://172.16.0.1?0=${networkData.network_id}&1=${networkData.refresh_token}&2=${ssid}&3=${pass}`}
+                target="_blank">
+                Done
+              </Button>}
+            {state == State.SelectWifiNetwork
               && <Button
                 onClick={handleClose}
                 href={`http://172.16.0.1?0=${networkData.network_id}&1=${networkData.refresh_token}&2=${ssid}&3=${pass}`}
